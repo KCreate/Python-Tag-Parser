a9bfce45d728bc659db7b6cd6b5ca87d4215cb34
Leonard Schuetz <leni.schuetz13@gmail.com>
2015-10-26 20:46:25 +0100

    Initial commit
    Initial commit

---
+++ tagparser.py
@@ -0,0 +1,12 @@
+import string
+import sys
+def p(t, s):
+    x = s
+    r = []
+    while s!="" and s.find(t)!=-1:
+        x=s[len(t)+s.find(t):]
+        x=x[:x.find(t)]
+        if x.isspace()!=1:r.append(x)
+        s = s[s.find(t+x+t)+len(t+x+t):] if t in s[len(t+x+t):] else ""
+    return r
+print(p(sys.argv[1], sys.argv[2]))
